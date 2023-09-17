9a26e059168e411a6ab583a2b34bca75d151d1ae
111368035 <111368035@ntut.edu.tw>
2023-09-18 00:12:37 +0800

    ans_for_data_finish

--- data/answer_for_channel.py
+++ data/answer_for_channel.py
@@ -2,39 +2,76 @@ import pandas as pd
 import numpy as np
 import re
 # 16point for h0 or h1
-point16_h0 = pd.read_csv('D:\\Desktop\\MLforSchool\\data\\constellations\\16qam_for_0\\16qam_10_15.csv')
-point16_h1 = pd.read_csv('D:\\Desktop\\MLforSchool\\data\\constellations\\16qam_for_1\\16qam_10_15.csv')
+point16_h0_csv = pd.read_csv('D:\\MLforSchool\\data\\constellations\\16qam_for_0\\16qam_10_15.csv')
+point16_h1_csv = pd.read_csv('D:\\MLforSchool\\data\\constellations\\16qam_for_1\\16qam_10_150.csv')
 #random_feature = pd.read_csv('D:\\MLforSchool\\data\\16qam_test\\random_feature100_forTest.csv')
 #random_feature = pd.read_csv('D:\\MLforSchool\\data\\16qam_train\\random_feature1000_forTrain.csv')
-random_feature = pd.read_csv('D:\\Desktop\\MLforSchool\\data\\QPSK_for_channel\\U.csv')
+random_feature_csv = pd.read_csv('D:\\MLforSchool\\data\\16qam_for_channel\\fU_for_test.csv')
 
 
-point16_h0.replace('i', 'j', regex=True, inplace=True)
-point16_h1.replace('i', 'j', regex=True, inplace=True)
-random_feature.replace('i', 'j', regex=True, inplace=True)
+point16_h0_csv.replace('i', 'j', regex=True, inplace=True)
+point16_h1_csv.replace('i', 'j', regex=True, inplace=True)
+random_feature_csv.replace('i', 'j', regex=True, inplace=True)
 
 def point16_h(row):
     return [complex(row[str(j)]) for j in range(1, 9)]
 
-results = [[] for _ in range(4)]
+def cal_distance(a, b):
+    return abs(a - b)
 
-for j, _ in random_feature.iterrows():
-    d0 = []
-    d1 = []
-    for i, row_h0 in point16_h0.iterrows():
-        h0 = point16_h(row_h0)    
-        d0_row = (abs(data - complex(val)) for data, val in zip(h0, range(1, 11)))  # for b = 0
-        d0.append(min(d0_row))
-    for i, row_h1 in point16_h1.iterrows():
-        h1 = point16_h(row_h1)
-        d1_row = (abs(data - complex(val)) for data, val in zip(h1, range(1,11)))  # for b = 1
-        d1.append(min(d1_row))
-    for i in range(len(d0)):
-        results.append(d1[i] - d0[i])
-        
-        
-        fUi = pd.DataFrame(results[i], columns=['ans'])
-        fUi.to_csv(f'D:\\Desktop\\MLforSchool\\data\\QPSK_for_channel\\result_i_{i}.csv', index=False)
+# {
+#   0: [ans, ans, ans...],
+#   1: [ans, ans, ans...],
+#    ...
+# }
+dict_for_bit_ans = {}
+
+for j, row_rf in random_feature_csv.iterrows():
+
+    list_rf = row_rf.values.tolist()
+    list_rf.pop(0)
+    for random_feature_item in list_rf:
+        for i, row_h0 in point16_h0_csv.iterrows():
+            row_h1 = point16_h1_csv.iloc[i]
+            list_h0 = row_h0.values.tolist()
+            list_h1 = row_h1.values.tolist()
+            list_h0.pop(0)
+            list_h1.pop(0)
+
+            def cal_distance_of_random_feature_item(item):
+                return cal_distance(complex(item), complex(random_feature_item))
+            def cal_min_distance_of_random_feature_item(array):
+                return min(list(map(cal_distance_of_random_feature_item, array)))
+                
+            min_h0 = cal_min_distance_of_random_feature_item(list_h0)
+            min_h1 = cal_min_distance_of_random_feature_item(list_h1)
+            fUi = min_h1 - min_h0
+            if i not in dict_for_bit_ans:
+                dict_for_bit_ans[i] = []
+            
+            dict_for_bit_ans[i].append(fUi)
+            
+# print(dict_for_bit_ans)
+# 整理成原本random_feature.csv格式
+for key in dict_for_bit_ans.keys():
+    result = {}
+    index = 0
+    for item in dict_for_bit_ans[key]:
+        if index not in result:
+            result[index] = []
+        result[index].append(item)
+        if (len(result[index]) >= 10):
+            index = index + 1
+
+    csv = pd.DataFrame(result)                
+    csv.T.to_csv(f'D:\\MLforSchool\\data\\16qam_for_channel\\result_{key}.csv')
+
+
+
+
+
+# fUi = pd.DataFrame(results[i], columns='ans')
+# fUi.to_csv(f'D:\\MLforSchool\\data\\16qam_for_channel\\result_i_{i}.csv', index=False)
 
         
 
