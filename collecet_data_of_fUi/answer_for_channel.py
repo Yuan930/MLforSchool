d432467f3e1848a61c3307d9a0a35529e694e551
Yuan0930 <tingyuan0930@gmail.com>
2023-09-18 18:46:07 +0800

    renew ans_for_random_feature

---
+++ collecet_data_of_fUi/answer_for_channel.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import numpy as np
+import re
+# 16point for h0 or h1
+point16_h0_csv = pd.read_csv('D:\\MLforSchool\\data\\constellations\\16qam_for_0\\16qam_10_15.csv')
+point16_h1_csv = pd.read_csv('D:\\MLforSchool\\data\\constellations\\16qam_for_1\\16qam_10_15.csv')
+#random_feature = pd.read_csv('D:\\MLforSchool\\data\\16qam_test\\random_feature100_forTest.csv')
+#random_feature = pd.read_csv('D:\\MLforSchool\\data\\16qam_train\\random_feature1000_forTrain.csv')
+random_feature_csv = pd.read_csv('D:\\MLforSchool\\data\\16qam_for_channel\\fU_for_test.csv')
+
+
+point16_h0_csv.replace('i', 'j', regex=True, inplace=True)
+point16_h1_csv.replace('i', 'j', regex=True, inplace=True)
+random_feature_csv.replace('i', 'j', regex=True, inplace=True)
+
+def point16_h(row):
+    return [complex(row[str(j)]) for j in range(1, 9)]
+
+def cal_distance(a, b):
+    return abs(a - b)
+
+# {
+#   0: [ans, ans, ans...],
+#   1: [ans, ans, ans...],
+#    ...
+# }
+dict_for_bit_ans = {}
+
+for j, row_rf in random_feature_csv.iterrows():
+
+    list_rf = row_rf.values.tolist()
+    list_rf.pop(0)
+    for random_feature_item in list_rf:
+        for i, row_h0 in point16_h0_csv.iterrows():
+            row_h1 = point16_h1_csv.iloc[i]
+            list_h0 = row_h0.values.tolist()
+            list_h1 = row_h1.values.tolist()
+            list_h0.pop(0)
+            list_h1.pop(0)
+
+            def cal_distance_of_random_feature_item(item):
+                return cal_distance(complex(item), complex(random_feature_item))
+            def cal_min_distance_of_random_feature_item(array):
+                return min(list(map(cal_distance_of_random_feature_item, array)))
+                
+            min_h0 = cal_min_distance_of_random_feature_item(list_h0)
+            min_h1 = cal_min_distance_of_random_feature_item(list_h1)
+            fUi = min_h1 - min_h0
+            if i not in dict_for_bit_ans:
+                dict_for_bit_ans[i] = []
+            
+            dict_for_bit_ans[i].append(fUi)
+            
+print(dict_for_bit_ans)
+# 整理成原本random_feature.csv格式
+for key in dict_for_bit_ans.keys():
+    result = {}
+    index = 0
+    for item in dict_for_bit_ans[key]:
+        if index not in result:
+            result[index] = []
+        result[index].append(item)
+        if (len(result[index]) >= 10):              #根據測試資料的行數更改
+            index = index + 1
+
+    csv = pd.DataFrame(result)                
+    csv.T.to_csv(f'D:\\MLforSchool\\data\\16qam_for_channel\\result_{key}.csv')
+
+
+
+
+
+# fUi = pd.DataFrame(results[i], columns='ans')
+# fUi.to_csv(f'D:\\MLforSchool\\data\\16qam_for_channel\\result_i_{i}.csv', index=False)
+
+        
+
+
+        
+
---
+++ collecet_data_of_fUi/answer_for_randomfeature.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import numpy as np
+import re
+# 16point for h0 or h1
+point16_h0_csv = pd.read_csv('D:\\MLforSchool\\data\\constellations\\16qam_for_0\\16qam_10_15.csv')
+point16_h1_csv = pd.read_csv('D:\\MLforSchool\\data\\constellations\\16qam_for_1\\16qam_10_15.csv')
+#random_feature = pd.read_csv('D:\\MLforSchool\\data\\16qam_test\\random_feature100_forTest.csv')
+#random_feature = pd.read_csv('D:\\MLforSchool\\data\\16qam_train\\random_feature1000_forTrain.csv')
+random_feature_csv = pd.read_csv('D:\\MLforSchool\\data\\16qam_for_randomfeature\\16qam_valid\\random_feature100_forVaild.csv')
+
+
+point16_h0_csv.replace('i', 'j', regex=True, inplace=True)
+point16_h1_csv.replace('i', 'j', regex=True, inplace=True)
+
+def point16_h(row):
+    return [complex(row[str(j)]) for j in range(1, 9)]
+def cal_distance(a,b):
+    return abs(a-b)
+
+results = []
+for j, row_rf in random_feature_csv.iterrows():
+    d0 = []
+    d1 = []
+    
+    def cal_distance_of_random_feature_item(h):
+        return min(cal_distance(data, complex(row_rf['complex'])) for data in h)
+    
+    for i, row_h0 in point16_h0_csv.iterrows():
+        h0 = point16_h(row_h0)         
+        d0.append(cal_distance_of_random_feature_item(h0))  #for bk = 0
+        
+    for i, row_h1 in point16_h1_csv.iterrows():
+        h1 = point16_h(row_h1)       
+        d1.append(cal_distance_of_random_feature_item(h1))  #for bk = 1
+    
+    for i in range(len(d0)):
+        results.append(d1[i] - d0[i])
+     
+#整理成想要的資料存放形式 
+bit = 4  
+results_matrix = {}
+index = 0
+for item in results:   
+    if index not in results_matrix:
+        results_matrix[index] = []
+    results_matrix[index].append(item)
+    if (len(results_matrix[index]) >= bit):
+        index = index + 1
+print(results_matrix)        
+    
+       
+
+# result_matrix = []
+# # 每行四個答案
+# num_rows = len(results) // 4
+# result_matrix = np.array(results[:4 * num_rows]).reshape(num_rows, 4)
+csv = pd.DataFrame(results_matrix)
+csv.index = [f'b{i}' for i in range(bit)]
+#generate_fUi = fUi.to_csv('D:\\MLforSchool\\data\\16qam_train\\train_10_15_1000.csv') #train
+#generate_fUi = fUi.to_csv('D:\\MLforSchool\\data\\16qam_test\\ans_for_test\\actual_ans_10_15_100.csv') #test
+csv.T.to_csv('D:\\MLforSchool\\data\\16qam_for_randomfeature\\16qam_valid\\valid_10_15_1000.csv') #valid
+
+
