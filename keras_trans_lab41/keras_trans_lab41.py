13ae067a9f6875947621621caeb5ff97c26d1fb6
Yuan <tingyuan0930@gmail.com>
2023-09-27 17:00:07 +0800

    count positive and negative

--- keras_trans_lab41/keras_trans_lab41.py
+++ keras_trans_lab41/keras_trans_lab41.py
@@ -8,29 +8,33 @@ Created on Fri Dec 10 01:30:16 2021
 import pandas as pd
 import numpy as np
 
-data_1 = pd.read_csv('D://MLforSchool//dnn_experiments//independent_ans//mlp_predict_answer_lab2_16qam_10_15_100_1.4_senior_b2.csv')
+data_1 = pd.read_csv('D://MLforSchool//dnn_experiments//independent_ans//mlp_predict_answer_lab2_16qam_10_15_100_1.4_senior.csv')
 #data_2 = pd.read_csv('C://Users//oscar//Desktop//experiments//1208lab2.5//rbf//predict_answer_lab2.5_rbfcsv.csv')
 test_nums = 2   ##28p4:1 28p8:2
 
-print(data_1.iloc[0,1])
-print(data_1)
+def positive_and_negative_count(a):
+    positive_count = (a > 0).sum()
+    negative_count = (a < 0).sum()
+    return print(f'正:\n{positive_count}',f'負:\n{negative_count}')
+
 string = "Hey! What's up?"
 characters = "'!?"
 
 character = "[]"
 #print(data_1.iloc[0,4])
 for i in range(0,100):#1684
-    #for j in range(1,5):
-    for j in range(1,2):    
+    for j in range(1,5):    
         data_1.iloc[i,j] = ''.join( x for x in data_1.iloc[i,j] if x not in character)
 print(data_1)
-data_1 = data_1.iloc[:,1]
+data_1 = data_1.iloc[:,1:5]
 print(data_1)
 data_3 = data_1.astype(float)
-positive_count = (data_3 > 0).sum()
-negative_count = (data_3 < 0).sum()
-print(f'正: {positive_count}')
-print(f'負: {negative_count}')
+print('data3',data_3)
+count = positive_and_negative_count(data_3)
+
+
+
+
 
 data2 = data_1
 # aa = data_1.to_csv('D://MLforSchool//dnn_experiments//independent_ans//lab2_trans_answer_mlp_16qam_10_15_100_1.4_neurous15_b2.csv')
