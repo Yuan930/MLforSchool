23df633e7f7493ed2e95bac77fb942e76061c2a5
Yuan <tingyuan0930@gmail.com>
2023-10-01 23:22:57 +0800

    象限壓縮示意程式

--- reduced_to_first_quadrant/for_cal_fUi_and_for_DNN_to_predict.py
+++ reduced_to_first_quadrant/for_cal_fUi_and_for_DNN_to_predict.py
@@ -1,26 +1,27 @@
 import pandas as pd
 import re
-# 假設你有一個 DataFrame，包含複數數據
-feature_in_channel_csv = pd.read_csv('D:\\MLforSchool\\data\\16qam_for_channel\\16qam_test\\fU_for_test.csv')
 
-# 定義將複數變為絕對值的函數
+data = 'train'  #train valid test
+
+# 假設你有一個 DataFrame，包含複數數據
+feature_in_channel_csv = pd.read_csv(f'D:\\MLforSchool\\data\\16qam_for_channel\\16qam_{data}\\fU_for_{data}.csv')
+
+# 將複數變為絕對值的函數
 def make_positive(x):
     return complex(abs(x.real), abs(x.imag))
+# 由於python的複數是j，所以要把i變為j
 def change_i_to_j(x):
     return complex(x.replace('i', 'j'))
+# 去除括號
 def remove_parentheses(x):
     return re.sub(r'[()]', '', str(x))
+# 實虛分開
 def split_real_and_imag(x):
-    return pd.Series([x.real,x.imag])
+    return f'[{x.real},{x.imag}]'
 feature_in_channel_csv = feature_in_channel_csv.iloc[0:, 1:].applymap(change_i_to_j)
-# 將所有複數變為其絕對值
+# 將所有複數取絕對值(象限壓縮)
 transform_to_positive = feature_in_channel_csv.applymap(make_positive)
 # 去除括號
 remove_parentheses_transform_to_positive = transform_to_positive.applymap(remove_parentheses)
 
-# transform_to_positive.to_csv('D:\\MLforSchool\\data\\16qam_for_channel\\16qam_test\\fU_for_test_positive.csv')
-
-split_transform_to_positive = transform_to_positive.applymap(split_real_and_imag)
-split_transform_to_positive.to_csv('D:\\MLforSchool\\data\\16qam_for_channel\\16qam_test\\fU_for_test_split.csv')
-print(split_transform_to_positive)
-
+remove_parentheses_transform_to_positive.to_csv(f'D:\\MLforSchool\\data\\16qam_for_channel\\16qam_{data}\\fU_for_{data}_positive.csv')
