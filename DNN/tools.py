fda34827dee4323b416ab2ed9c25fdac4224de9d
Yuan0930 <tingyuan0930@gmail.com>
2024-07-12 17:02:20 +0800

    複數分離

--- DNN/tools.py
+++ DNN/tools.py
@@ -1,5 +1,5 @@
 import re
-
+import numpy as np
 def flatten(l):
      return [item for sublist in l for item in sublist]
 
@@ -12,6 +12,12 @@ def remove_parentheses(x):
 def change_all_positive(x):
     return remove_parentheses(str(complex(abs(x.real), abs(x.imag))))
 
-def split_real_and_imag(comp):
+def split_real_and_imag_allpositive(comp):
     array = comp.replace('j', '').split('+')
     return [float(array[0]), float(array[1])]
+
+def split_real_and_imag(complex_array):
+    real_part = complex_array.real
+    imag_part = complex_array.imag
+    result = np.vstack((real_part, imag_part)).T.tolist()
+    return result
